3426fb20ed55c680fd803ef130562af3a4951114
Yotoz <thdrudwls7@gmail.com>
2020-08-14 03:34:04 +0900

    Add Boss Attack

--- src/componants/GameScreen/index.js
+++ src/componants/GameScreen/index.js
@@ -1,27 +1,50 @@
-import React, { useState } from 'react';
-import './styles.scss'
+import React, { useState, useEffect } from "react";
+import "./styles.scss";
+
+//32 13
+const KEY_SPACE = 32;
+const KEY_ENTER = 13;
 
 const GameScreen = () => {
-    const [bossSize, setBossSize] = useState(500);
+  const [bossSize, setBossSize] = useState(500);
 
-    const bossStyle = {
-        width: bossSize,
-        height: bossSize,
-        borderRadius: bossSize / 2
+  const bossStyle = {
+    width: bossSize,
+    height: bossSize,
+    borderRadius: bossSize / 2,
+  };
+
+  useEffect(() => {
+    const keyupHandler = (e) => {
+      switch (e.keyCode) {
+        case KEY_SPACE:
+        case KEY_ENTER:
+          setBossSize(bossSize - 5);
+      }
     };
-    
-    return (
-        <div className='game-screen'>
-            <div className='game-monitor'>
-                <div className='boss' style={bossStyle}>
 
-                </div>
-            </div>
-            <div className='guideLine'>
-                ^^
-            </div>
-        </div>
-    );
+    window.addEventListener("keyup", keyupHandler);
+
+    return () => {
+      window.removeEventListener("keyup", keyupHandler);
+    };
+  }, [bossSize]);
+
+  useEffect(() => {
+    if (bossSize < 0) {
+      const newBossSize = Math.floor(Math.random() * 600 + 100);
+      setBossSize(newBossSize);
+    }
+  }, [bossSize]);
+
+  return (
+    <div className="game-screen">
+      <div className="game-monitor">
+        <div className="boss" style={bossStyle}></div>
+      </div>
+      <div className="guideLine">^^</div>
+    </div>
+  );
 };
 
 export default GameScreen;
