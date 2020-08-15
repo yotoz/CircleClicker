fdc2882b2e2bf10301da595bcfcac3ad4c6a521f
Yotoz <thdrudwls7@gmail.com>
2020-08-16 04:35:25 +0900

    REMOVE WARNINGS

--- src/components/HitDamage/index.js
+++ src/components/HitDamage/index.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useRef } from "react";
+import React, { useEffect, useRef } from "react";
 import "./styles.scss";
 
 const HitDamage = ({ damage, left, top }) => {
