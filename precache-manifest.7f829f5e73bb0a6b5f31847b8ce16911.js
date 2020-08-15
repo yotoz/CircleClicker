606004dde3279cc16db55f578132183945c6f277
Yotoz <thdrudwls7@gmail.com>
2020-08-16 04:48:02 +0900

    Updates

---
+++ precache-manifest.7f829f5e73bb0a6b5f31847b8ce16911.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9ebf892f8da1020047af7a2e527209ee",
+    "url": "/CircleClicker/index.html"
+  },
+  {
+    "revision": "d79ae5edae533999fab7",
+    "url": "/CircleClicker/static/css/main.e72bd18e.chunk.css"
+  },
+  {
+    "revision": "88b8f4f577207e6ce636",
+    "url": "/CircleClicker/static/js/2.40040e74.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/CircleClicker/static/js/2.40040e74.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d79ae5edae533999fab7",
+    "url": "/CircleClicker/static/js/main.1efba224.chunk.js"
+  },
+  {
+    "revision": "df25431e9bededb200c8",
+    "url": "/CircleClicker/static/js/runtime-main.f747f656.js"
+  }
+]);
